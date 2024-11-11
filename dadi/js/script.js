51dc5e14038ed62311179204ae4619100b031283
Karnjyot <krnjyotsingh2004@email.com>
2024-11-11 13:35:02 +0100

    feat: fine esercizio

---
+++ dadi/js/script.js
@@ -0,0 +1,15 @@
+//DADI
+const playerDice = Math.floor(Math.random() * 6) + 1;
+const npcDice = Math.floor(Math.random() * 6) + 1;
+
+console.log(`Dado Giocatore: ${playerDice}
+Dado Avversario: ${npcDice}`);
+
+let message = "";
+if (playerDice > npcDice) {
+    message = "Congratulazioni hai vinto!";
+} else if (playerDice === npcDice) {
+    message = "Pareggio! Eri ad un soffio dalla vittoria!";
+} else {
+    message = "Sconfitta! Riprova sarai piu' fortunato!";
+}
